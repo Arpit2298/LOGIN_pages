77d1f0abbb93a2f7ede6cb86e33aa0f2d3278d47
Arpit2298 <73097367+Arpit2298@users.noreply.github.com>
2021-02-04 17:43:55 +0530

    new 2 logins

---
+++ Lo-re2.js
@@ -0,0 +1,12 @@
+function demo() {
+    document.querySelector('.mainbox1').style.top = "-250px";
+    document.querySelector('#sign').style.top = "30px";
+    document.querySelector('.mainbox1').style.backgroundColor = "  royalblue";
+
+}
+
+function demo1() {
+    document.querySelector('.mainbox1').style.top = "80px";
+    document.querySelector('#sign').style.top = "100px";
+    document.querySelector('.mainbox1').style.backgroundColor = "rgba(95, 158, 160, 0.466)";
+}
